26cac7db8a8a051199338e46b3089242f1d2e241
zgldh <zgldh@hotmail.com>
2012-12-12 22:36:36 +0800

    sql

--- application/patch/tasklist.sql
+++ application/patch/tasklist.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50528
 File Encoding         : 65001
 
-Date: 2012-12-12 00:01:57
+Date: 2012-12-12 22:36:26
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -31,13 +31,13 @@ CREATE TABLE `command` (
 -- ----------------------------
 -- Records of command
 -- ----------------------------
-INSERT INTO `command` VALUES ('8', '16', 'url-request', '{\"url\":\"http:\\/\\/www.zgldh.com\"}', '2012-12-11 21:23:53');
-INSERT INTO `command` VALUES ('9', '17', 'send-email', '{\"recipients\":\"test@email.com;\",\"content\":\"none\"}', '2012-12-11 21:23:19');
-INSERT INTO `command` VALUES ('10', '18', 'url-request', '{\"url\":\"asdfasdf\"}', '2012-12-11 23:03:14');
-INSERT INTO `command` VALUES ('11', '19', 'url-request', '{\"url\":\"http:\\/\\/www.sina.com\"}', '2012-12-09 23:46:17');
-INSERT INTO `command` VALUES ('12', '20', 'send-email', '{\"recipients\":\"test@email.com;\",\"content\":\"++\"}', '2012-12-09 23:47:49');
-INSERT INTO `command` VALUES ('14', '21', 'send-email', '{\"recipients\":\"test@email.com;\",\"content\":\"aaa\"}', '2012-12-10 23:58:31');
-INSERT INTO `command` VALUES ('15', '16', 'send-email', '{\"recipients\":\"test@email.com;\",\"content\":\"&nbsp;\\u5e76\\u4e0d<br \\/>\"}', '2012-12-11 21:23:53');
+INSERT INTO `command` VALUES ('8', '16', 'url-request', '{\"url\":\"http:\\/\\/www.zgldh.com\"}', '2012-12-12 21:39:51');
+INSERT INTO `command` VALUES ('9', '17', 'send-email', '{\"recipients\":\"test@email.com;\",\"content\":\"none\"}', '2012-12-12 22:34:18');
+INSERT INTO `command` VALUES ('10', '18', 'url-request', '{\"url\":\"asdfasdf\"}', '2012-12-12 21:39:59');
+INSERT INTO `command` VALUES ('11', '19', 'url-request', '{\"url\":\"http:\\/\\/www.sina.com\"}', '2012-12-12 21:40:01');
+INSERT INTO `command` VALUES ('12', '20', 'send-email', '{\"recipients\":\"test@email.com;\",\"content\":\"++\"}', '2012-12-12 21:40:11');
+INSERT INTO `command` VALUES ('14', '21', 'send-email', '{\"recipients\":\"test@email.com;\",\"content\":\"aaa\"}', '2012-12-12 21:40:07');
+INSERT INTO `command` VALUES ('15', '16', 'send-email', '{\"recipients\":\"test@email.com;\",\"content\":\"\\u00a0\\u5e76\\u4e0d<br \\/>\"}', '2012-12-12 21:39:51');
 
 -- ----------------------------
 -- Table structure for `condition`
@@ -55,12 +55,12 @@ CREATE TABLE `condition` (
 -- ----------------------------
 -- Records of condition
 -- ----------------------------
-INSERT INTO `condition` VALUES ('16', '16', 'date-static', '{\"year\":\"2013\",\"month\":\"\",\"day\":\"\",\"hour\":\"0\"}', '2012-12-11 21:23:53');
-INSERT INTO `condition` VALUES ('17', '17', 'date-static', '{\"year\":\"2013\",\"month\":\"2\",\"day\":\"\",\"hour\":\"0\"}', '2012-12-11 21:23:19');
-INSERT INTO `condition` VALUES ('18', '18', 'date-static', '{\"year\":\"2014\",\"month\":\"\",\"day\":\"31\",\"hour\":\"0\"}', '2012-12-11 23:03:14');
-INSERT INTO `condition` VALUES ('19', '19', 'date-static', '{\"year\":\"\",\"month\":\"8\",\"day\":\"\",\"hour\":\"2\"}', '2012-12-09 23:46:17');
-INSERT INTO `condition` VALUES ('20', '20', 'date-static', '{\"year\":\"\",\"month\":\"10\",\"day\":\"10\",\"hour\":\"0\"}', '2012-12-09 23:47:49');
-INSERT INTO `condition` VALUES ('21', '21', 'date-static', '{\"year\":\"2015\",\"month\":\"1\",\"day\":\"3\",\"hour\":\"0\"}', '2012-12-10 23:58:31');
+INSERT INTO `condition` VALUES ('16', '16', 'date-static', '{\"year\":\"2013\",\"month\":\"\",\"day\":\"\",\"hour\":\"0\"}', '2012-12-12 21:39:51');
+INSERT INTO `condition` VALUES ('17', '17', 'date-static', '{\"year\":\"2013\",\"month\":\"2\",\"day\":\"\",\"hour\":\"0\"}', '2012-12-12 22:34:18');
+INSERT INTO `condition` VALUES ('18', '18', 'date-static', '{\"year\":\"2014\",\"month\":\"\",\"day\":\"31\",\"hour\":\"0\"}', '2012-12-12 21:39:59');
+INSERT INTO `condition` VALUES ('19', '19', 'date-static', '{\"year\":\"\",\"month\":\"8\",\"day\":\"\",\"hour\":\"2\"}', '2012-12-12 21:40:01');
+INSERT INTO `condition` VALUES ('20', '20', 'date-static', '{\"year\":\"\",\"month\":\"10\",\"day\":\"10\",\"hour\":\"0\"}', '2012-12-12 21:40:11');
+INSERT INTO `condition` VALUES ('21', '21', 'date-static', '{\"year\":\"2015\",\"month\":\"1\",\"day\":\"3\",\"hour\":\"0\"}', '2012-12-12 21:40:07');
 
 -- ----------------------------
 -- Table structure for `invitation`
@@ -120,11 +120,11 @@ CREATE TABLE `task` (
 -- Records of task
 -- ----------------------------
 INSERT INTO `task` VALUES ('16', '12', 'active', 'Task 16', '0', '0', '2012-12-09 23:39:23', '2012-12-09 23:39:23');
-INSERT INTO `task` VALUES ('17', '12', 'active', 'Task 17', '0', '0', '2012-12-09 23:41:58', '2012-12-09 23:41:58');
+INSERT INTO `task` VALUES ('17', '12', 'pause', 'Task 17', '0', '0', '2012-12-09 23:41:58', '2012-12-09 23:41:58');
 INSERT INTO `task` VALUES ('18', '12', 'active', 'Task 18', '22', '0', '2012-12-09 23:45:30', '2012-12-09 23:45:30');
-INSERT INTO `task` VALUES ('19', '12', 'active', 'lol', '0', '0', '2012-12-09 23:46:17', '2012-12-09 23:46:17');
-INSERT INTO `task` VALUES ('20', '12', 'active', '<b>sdfds</b>', '0', '0', '2012-12-09 23:47:49', '2012-12-10 00:22:00');
-INSERT INTO `task` VALUES ('21', '12', 'active', 'Task 212', '998', '0', '2012-12-09 23:54:24', '2012-12-09 23:54:24');
+INSERT INTO `task` VALUES ('19', '12', 'pause', 'lol', '0', '0', '2012-12-09 23:46:17', '2012-12-09 23:46:17');
+INSERT INTO `task` VALUES ('20', '12', 'pause', '<b>sdfds</b>', '0', '0', '2012-12-09 23:47:49', '2012-12-09 23:47:49');
+INSERT INTO `task` VALUES ('21', '12', 'pause', 'Task 212', '998', '0', '2012-12-09 23:54:24', '2012-12-09 23:54:24');
 
 -- ----------------------------
 -- Table structure for `timing_process`
@@ -142,11 +142,28 @@ CREATE TABLE `timing_process` (
   KEY `plan_time` (`plan_time`,`skip`),
   KEY `gen_time` (`gen_time`),
   KEY `task_id` (`task_id`,`plan_time`) USING BTREE
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=FIXED;
+) ENGINE=MyISAM AUTO_INCREMENT=96 DEFAULT CHARSET=utf8 ROW_FORMAT=FIXED;
 
 -- ----------------------------
 -- Records of timing_process
 -- ----------------------------
+INSERT INTO `timing_process` VALUES ('95', '18', '', '', '2012-12-12 22:34:29', null, '2014-12-31 00:00:00');
+INSERT INTO `timing_process` VALUES ('94', '18', '', '', '2012-12-12 22:34:29', null, '2014-10-31 00:00:00');
+INSERT INTO `timing_process` VALUES ('93', '18', '', '', '2012-12-12 22:34:29', null, '2014-08-31 00:00:00');
+INSERT INTO `timing_process` VALUES ('92', '18', '', '', '2012-12-12 22:34:29', null, '2014-07-31 00:00:00');
+INSERT INTO `timing_process` VALUES ('91', '18', '', '', '2012-12-12 22:34:29', null, '2014-05-31 00:00:00');
+INSERT INTO `timing_process` VALUES ('90', '18', '', '', '2012-12-12 22:34:29', null, '2014-03-31 00:00:00');
+INSERT INTO `timing_process` VALUES ('89', '18', '', '', '2012-12-12 22:34:29', null, '2014-01-31 00:00:00');
+INSERT INTO `timing_process` VALUES ('78', '16', '', '', '2012-12-12 22:34:23', null, '2013-01-10 00:00:00');
+INSERT INTO `timing_process` VALUES ('77', '16', '', '', '2012-12-12 22:34:23', null, '2013-01-09 00:00:00');
+INSERT INTO `timing_process` VALUES ('76', '16', '', '', '2012-12-12 22:34:23', null, '2013-01-08 00:00:00');
+INSERT INTO `timing_process` VALUES ('75', '16', '', '', '2012-12-12 22:34:23', null, '2013-01-07 00:00:00');
+INSERT INTO `timing_process` VALUES ('74', '16', '', '', '2012-12-12 22:34:23', null, '2013-01-06 00:00:00');
+INSERT INTO `timing_process` VALUES ('73', '16', '', '', '2012-12-12 22:34:23', null, '2013-01-05 00:00:00');
+INSERT INTO `timing_process` VALUES ('72', '16', '', '', '2012-12-12 22:34:23', null, '2013-01-04 00:00:00');
+INSERT INTO `timing_process` VALUES ('71', '16', '', '', '2012-12-12 22:34:23', null, '2013-01-03 00:00:00');
+INSERT INTO `timing_process` VALUES ('70', '16', '', '', '2012-12-12 22:34:23', null, '2013-01-02 00:00:00');
+INSERT INTO `timing_process` VALUES ('69', '16', '', '', '2012-12-12 22:34:23', null, '2013-01-01 00:00:00');
 
 -- ----------------------------
 -- Table structure for `user`
@@ -170,4 +187,4 @@ INSERT INTO `user` VALUES ('8', 'test1', '746927de0a4cb6be835d299e761e12e4', 'te
 INSERT INTO `user` VALUES ('9', 'test2', '746927de0a4cb6be835d299e761e12e4', 'test2@email.com', '2012-11-28 06:12:07', '39546989e37910d93a9c93a566e6eaf11354679578', '2013-01-04 00:00:00');
 INSERT INTO `user` VALUES ('10', 'test3', '746927de0a4cb6be835d299e761e12e4', 'test3@email.com', '2012-11-28 06:12:16', null, null);
 INSERT INTO `user` VALUES ('11', 'test4', '746927de0a4cb6be835d299e761e12e4', 'test4@email.com', '2012-11-28 06:12:25', null, null);
-INSERT INTO `user` VALUES ('12', 'test', '746927de0a4cb6be835d299e761e12e4', 'test@email.com', '2012-11-28 06:12:32', '4cd73879be6ea9f8310f7ac7704722b01355232180', '2013-01-10 00:00:00');
+INSERT INTO `user` VALUES ('12', 'test', '746927de0a4cb6be835d299e761e12e4', 'test@email.com', '2012-11-28 06:12:32', '62a871395c9b55e1a7f7e7a8e34723321355319515', '2013-01-11 00:00:00');
