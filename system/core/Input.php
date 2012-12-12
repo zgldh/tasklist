f89f2d0989fc9c7b916381fbd4c01a85dd036658
zgldh <zgldh@hotmail.com>
2012-12-12 18:57:02 +0800

    update core/input.php

--- system/core/Input.php
+++ system/core/Input.php
@@ -726,10 +726,11 @@ class CI_Input {
 	*/
 	function _clean_input_keys($str)
 	{
-		if ( ! preg_match("/^[a-z0-9:_\/-]+$/i", $str))
-		{
-			exit('Disallowed Key Characters.');
-		}
+        $config = &get_config('config');
+        if ( ! preg_match("/^[".$config['permitted_uri_chars']."]+$/i", rawurlencode($str)))
+        {
+            exit('Disallowed Key Characters.');
+        }
 
 		// Clean UTF-8 if supported
 		if (UTF8_ENABLED === TRUE)
